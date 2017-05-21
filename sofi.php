4a1edc39c8b05578fb22beee1e35e532ad13aa4f
anthones <antoniedimovski@gmail.com>
2017-05-21 23:27:04 +0200

    Add files via upload

---
+++ sofi.php
@@ -0,0 +1,116 @@
+<?php
+function izbrisi_niza($vrednost, $niza) {
+  return array_diff($niza, array($vrednost));
+}
+  $boi = array('White', 'Silver', 'Gray', 'Black', 'Red', 'Maroon', 'Yellow', 'Olive', 'Lime', 'Green', 'Aqua', 'Teal', 'Blue', 'Navy', 'Fuchsia', 'Purple');
+  $broj_boi = count($boi);
+  $pozadina_boja = $boi[rand(0,$broj_boi-1)];
+
+  $text_boi = izbrisi_niza($pozadina_boja, $boi);
+  $text_broj_boi = count($text_boi);
+  $prva_text_boja = $text_boi[rand(0,$text_broj_boi-1)];
+  $vtora_text_boja = $text_boi[rand(0,$text_broj_boi-1)];
+  $treta_text_boja = $text_boi[rand(0,$text_broj_boi-1)];
+  
+  $prva_font_golemina = (100 + (rand(0,20) * 5));
+  $vtora_font_golemina = (100 + (rand(0,20) * 5));
+  $treta_font_golemina = (100 + (rand(0,20) * 5));
+  
+  $poramnuvanja = array('center', 'left', 'right', 'justify');
+  $prvo_poramnuvanje = $poramnuvanja[rand(0,3)];
+  $vtoro_poramnuvanje = $poramnuvanja[rand(0,3)];
+  $treto_poramnuvanje = $poramnuvanja[rand(0,3)];
+  
+  $stilovi = array('bold', 'italic', 'underline', 'uppercase');
+  $prv_stil = $stilovi[rand(0,3)];
+  $vtor_stil = $stilovi[rand(0,3)];
+  $tret_stil = $stilovi[rand(0,3)];
+
+  $strana = $_SERVER['PHP_SELF'];
+  $sec = "1.5";
+?><!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $strana?>'">
+    <title>You are awesome!</title>
+    <style type="text/css">
+  body {
+    background: <?php echo $pozadina_boja; ?>;
+    margin: 0;
+    padding: 0;
+  }
+
+  .prva {
+    margin: 0;
+    padding: 0;
+   color: <?php echo $prva_text_boja; ?>;
+   font-size: <?php echo $prva_font_golemina; ?>px;
+   text-align: <?php echo $prvo_poramnuvanje; ?>;
+   <?php
+   switch($prv_stil) {
+     case 'bold':
+       echo "font-weight: bold;";
+       break;
+     case 'italic':
+       echo "font-style: italic;";
+       break;
+     case 'underline':
+       echo "text-decoration: underline;";
+       break;
+     case 'uppercase':
+       echo "text-transform: uppercase;";
+       break;
+    }?>
+  }
+  .vtora {
+    margin: 0;
+    padding: 0;
+     color: <?php echo $vtora_text_boja; ?>;
+     font-size: <?php echo $vtora_font_golemina; ?>px;
+     text-align: <?php echo $vtoro_poramnuvanje; ?>;
+     <?php
+     switch($vtor_stil) {
+       case 'bold':
+         echo "font-weight: bold;";
+         break;
+       case 'italic':
+         echo "font-style: italic;";
+         break;
+       case 'underline':
+         echo "text-decoration: underline;";
+         break;
+       case 'uppercase':
+         echo "text-transform: uppercase;";
+         break;
+         }?>
+  }
+  .treta {
+    color: <?php echo $treta_text_boja; ?>;
+    font-size: <?php echo $treta_font_golemina; ?>px;
+    text-align: <?php echo $treto_poramnuvanje; ?>;
+    <?php
+    switch($tret_stil) {
+      case 'bold':
+        echo "font-weight: bold;";
+        break;
+      case 'italic':
+        echo "font-style: italic;";
+        break;
+      case 'underline':
+        echo "text-decoration: underline;";
+        break;
+      case 'uppercase':
+        echo "text-transform: uppercase;";
+        break;
+    }
+    ?>
+  }
+    </style>
+  </head>
+  <body>
+    <p class="prva">Hey Sofi</p>
+    <p class="vtora">Don't Forget</p>
+    <p class="treta">You are awesome!</p>
+  </body>
+</html>
